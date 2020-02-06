9072e8eee3dd7e84f5bbdeecb96dd6d8adf6cef2
andry1709 <andry1709@ya.ru>
2020-02-06 21:54:07 +0400

    commit hello world

---
+++ src/main/java/ru/com/demo/controller/Controller.java
@@ -0,0 +1,19 @@
+package ru.com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
+                           Map<String, Object> model
+    ) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+}
